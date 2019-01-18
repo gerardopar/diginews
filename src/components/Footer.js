8f0cd58c3b19a5a2879472311ec8d667b82f3e07
Gerardo Paredes <gerardoparedes@gerardos-mbp.home>
2019-01-18 14:21:06 -0800

    media queries added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import news_api_logo from '../assets/img/news-api-logo.png';
 
 const Footer = () => {
     return (
         <div>
             <div className="footer">
-                <h5 className="footer__title">POWERED BY <a className="footer__link" href="https://newsapi.org/" target="_blank">NEWS API</a></h5>
-                <h5 className="footer__title">2018&copy; DIGI NEWS All Rights Reserved</h5>
+                <div className="footer__img--wrap">
+                    <a className="footer__link" href="https://newsapi.org/" target="_blank">
+                        <img src={news_api_logo} placeholder="news api logo" className="footer__img"/>
+                    </a>
+                </div>
+                <div>
+                    <h5 className="footer__title">POWERED BY <a className="footer__link" href="https://newsapi.org/" target="_blank">NEWS API</a></h5>
+                </div>
+                <div>
+                    <h5 className="footer__title">2019&copy; digi news All Rights Reserved</h5>
+                </div>
             </div>
         </div>
     );
